99a15184d55e905cfe25fa6a1575f26a2906ac9d
Ihago Freire Santos <ihago.santos@compasso.com.br>
2021-12-14 17:54:57 -0300

    jest dependence

--- src/adapters/in/http/controller/heroesController.ts
+++ src/adapters/in/http/controller/heroesController.ts
@@ -1,21 +1,20 @@
-import { Hero } from "../../../../core/domain/hero";
 import { FindAllHeroes } from "../../../../core/useCase/findAllHeroes";
 import { MemoryAdapter as DatabaseAdapter } from "../../../out/memoryAdapter";
 export class HeroesController {
 
-    async healthycheck(request, response) {
+    async healthycheck(request: any, response: any) {
         response.status(200).json({
             message: `the server is runing at port ${process.env.SERVER_PORT}`
         })
     }
 
-    async findAll(request, response): Promise<any> {
+    async findAll(request: any, response: any): Promise<any> {
         try {
             const { filter, limit, offset } = request.query;
 
             const databaseAdapter = new DatabaseAdapter();
             const findAllHeroes = new FindAllHeroes(databaseAdapter);
-            
+
             const result = await findAllHeroes.execute(filter, limit, offset);
             response.status(200).json(result)
 
